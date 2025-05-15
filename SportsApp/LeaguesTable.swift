3b4349cce8da8a82417d41fce0d03530f3ee163a
Shereen Mohamed <shereenmohamed923@gmail.com>
2025-05-15 14:23:10 +0300

    Creating Splash and initiating nibfiles

---
+++ SportsApp/LeaguesTable.swift
@@ -0,0 +1,20 @@
+//
+//  LeaguesTable.swift
+//  SportsApp
+//
+//  Created by Macos on 19/05/2025.
+//
+
+import UIKit
+
+class LeaguesTable: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ SportsApp/SplashViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SplashViewController.swift
+//  SportsApp
+//
+//  Created by Macos on 19/05/2025.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
