cacd056ba79a2e670243eef5b3f6e819a5750052
Shereen Mohamed <shereenmohamed923@gmail.com>
2025-05-23 19:21:47 +0300

    team details screen added

--- SportsApp/View/SplashView/SplashViewController.swift
+++ SportsApp/View/SplashView/SplashViewController.swift
@@ -1,28 +1,43 @@
-//
-//  SplashViewController.swift
-//  SportsApp
-//
-//  Created by Macos on 19/05/2025.
-//
-
 import UIKit
+import Lottie
 
 class SplashViewController: UIViewController {
+    
+    private var animationView: LottieAnimationView?
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
 
     override func viewDidLoad() {
-            super.viewDidLoad()
-        
-            DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) { [weak self] in
-                self?.navigateToHomeTabBar()
-            }
+        super.viewDidLoad()
+
+        setupLottieAnimation()
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) { [weak self] in
+            self?.navigateToHomeTabBar()
         }
+    }
 
-        private func navigateToHomeTabBar() {
-            let tabBarController = HomeTabBarViewController()
+    private func setupLottieAnimation() {
+        animationView = LottieAnimationView(name: "splash")
+        guard let animationView = animationView else { return }
 
-            if let navigationController = self.navigationController {
-                navigationController.setViewControllers([tabBarController], animated: true)
-            }
+        animationView.frame = view.bounds
+        animationView.contentMode = .scaleAspectFit
+        animationView.loopMode = .playOnce
+        animationView.animationSpeed = 1.0
+        view.addSubview(animationView)
+
+        animationView.play()
+    }
+
+    private func navigateToHomeTabBar() {
+        let tabBarController = HomeTabBarViewController()
+
+        if let navigationController = self.navigationController {
+            navigationController.setViewControllers([tabBarController], animated: true)
         }
-
+    }
 }
